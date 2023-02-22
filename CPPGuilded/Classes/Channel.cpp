738957189f2c7774464ae4352355ce8d4508276b
pakkographic <41129374+pakkographic@users.noreply.github.com>
2023-02-22 14:33:29 +0100

    update Channel.cpp

--- CPPGuilded/Classes/Channel.cpp
+++ CPPGuilded/Classes/Channel.cpp
@@ -12,12 +12,12 @@
 using namespace std;
 
 CPPGuilded::Channel::Channel(const json& data, Client* client) {
-    id = data.at("id").get<string>();
-    type = data.at("type").get<string>();
-    name = data.at("name").get<string>();
-    topic = data.at("topic").get<string>();
-    createdAt = data.at("createdAt").get<string>();
-    createdBy = data.at("createdBy").get<string>();
-    serverID = data.at("serverId").get<string>();
-    groupID = data.at("groupId").get<string>();
+    id = client->utils->get_value<std::string>(data, "id");
+    type = client->utils->get_value<std::string>(data, "type");
+    name = client->utils->get_value<std::string>(data, "name");
+    topic = client->utils->get_value<std::string>(data, "topic");
+    createdAt = client->utils->get_value<std::string>(data, "createdAt");
+    createdBy = client->utils->get_value<std::string>(data, "createdBy");
+    serverID = client->utils->get_value<std::string>(data, "serverId");
+    groupID = client->utils->get_value<std::string>(data, "groupId");
 };
